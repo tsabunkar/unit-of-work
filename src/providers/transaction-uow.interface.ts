ea2c17c5a0ad7f901c85dbbd677ab47eba919c82
ErrApparent <jed@errapparent.dev>
2022-02-28 11:18:33 -0800

    Add result propagation in TransactionUnitOfWork.complete

--- src/providers/transaction-uow.interface.ts
+++ src/providers/transaction-uow.interface.ts
@@ -1,6 +1,6 @@
 export interface TransactionUnitOfWork {
   start(): Promise<void>;
-  complete(work: () => void): Promise<void>;
+  complete<T>(work: () => T | Promise<T>): Promise<T>;
   registerRepository(repositry);
   registerCustomRepository(customRepository);
 }
